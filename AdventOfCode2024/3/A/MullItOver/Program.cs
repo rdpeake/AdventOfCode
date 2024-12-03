c212c7bb8618bd07305b721fd9f0412b7c8ea973
rdpeake <rdpeake@adam.com.au>
2024-12-03 22:57:52 +1030

    Completed day 3

---
+++ AdventOfCode2024/3/A/MullItOver/Program.cs
@@ -0,0 +1,11 @@
+﻿//load file
+using System.Text.RegularExpressions;
+
+
+var input = File.ReadAllText("./input.txt");
+var content = Regex.Matches(input, @"mul\(([0-9]{1,3}),([0-9]{1,3})\)");
+
+var result = content.Select(x => int.Parse(x.Groups[1].Value) * int.Parse(x.Groups[2].Value)).Sum();
+
+Console.WriteLine(result.ToString());
+
---
+++ AdventOfCode2024/3/B/MullItOver/Program.cs
@@ -0,0 +1,21 @@
+﻿//load file
+using System.Text.RegularExpressions;
+
+
+var input = File.ReadAllText("./input.txt");
+var content = Regex.Matches(input, @"do\(\)|don't\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\)");
+
+
+var enabled = true;
+var sum = 0;
+foreach (Match match in content)
+{
+    if (match.Value == "don't()") { enabled = false; continue; }
+    if (match.Value == "do()") { enabled = true; continue; }
+    if (enabled)
+    {
+        sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+    }
+}
+Console.WriteLine(sum.ToString());
+
