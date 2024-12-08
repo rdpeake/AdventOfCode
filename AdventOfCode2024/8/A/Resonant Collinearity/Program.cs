d7c5c0c32bbc90a940929e705647e4277a35d0e6
rdpeake <rdpeake@adam.com.au>
2024-12-09 02:21:53 +1030

    begin day 8

---
+++ AdventOfCode2024/8/A/Resonant Collinearity/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("./input.txt");
+var height = input.Length;
+var width = input[0].Length;
+
+var board = input.SelectMany((x, i) => x.Select((y, j) => new Antenna(y, j, i)));
+
+var activeAntenna = board.Where(x => Regex.IsMatch($"{x.Frequency}", "[0-9a-zA-Z]")).GroupBy(x => x.Frequency);
+
+
+
+
+
+
+
+
+class Antenna
+{
+    public char Frequency { get; private set; }
+    public List<char> Antinodes { get; private set; }
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Antenna(char frequency, int x, int y)
+    {
+        Frequency = frequency;
+        X = x;
+        Y = y;
+        Antinodes = new List<char>();
+    }
+}
